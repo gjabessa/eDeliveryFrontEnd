9d28b268da731d454592f9e5eb7784b2a399923c
thenewcreation <galatafir@yahoo.com>
2021-07-18 07:58:04 -0500

    layouts in progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { ItemsComponent } from './items/items.component';
+import { OrdersComponent } from './orders/orders.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component: WelcomeComponent},
+{path:"items/:id", component: ItemDetailComponent},
+{path: "orders", component: OrdersComponent},
+{path:"items", component: ItemsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { ItemsComponent } from './items/items.component';
+import { NavComponent } from './layout/nav/nav.component';
+import { OrdersComponent } from './orders/orders.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DisplayComponent } from './layout/display/display.component';
+import { ItemsListComponent } from './sections/items-list/items-list.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { SingleItemComponent } from './micro/single-item/single-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    ItemDetailComponent,
+    OrdersComponent,
+    WelcomeComponent,
+    NavComponent,
+    DisplayComponent,
+    ItemsListComponent,
+    FooterComponent,
+    SingleItemComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
